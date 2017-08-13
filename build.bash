a3b50b1bb41a950db98a117446eaa06bbdfc8452
Wolf <jt.wolf@outlook.com>
2017-08-13 12:11:39 -0400

    add sign

--- build.bash
+++ build.bash
@@ -4,35 +4,28 @@ device=
 todate='date +%Y%m%d'
 
 bdevice() {
-		# Ensure proper starting location
-		cd ~/lineage
+	# Ensure proper starting location
+	cd ~/lineage
 		
-		# Ensure final folder exists
-		if [ -d /var/www/html/build/$device/ ]; then
-			echo "Exists"
-		else
-			mkdir /var/www/html/build/$device/
-		fi
-		
-		# Breakfast
+	# Breakfast
         breakfast $device
 
         # Run build
-		mka target-files-package dist
+	mka target-files-package dist
 		
-		# Sign Build
-		~/lineage/build/tools/releasetools/sign_target_files_apks -o -d ~/.android-certs ~/lineage/out/dist/*-target_files-*.zip ~/lineage/signed-target_files.zip
-		
-		# Generate .zip file
-		~/lineage/build/tools/releasetools/ota_from_target_files -k ~/.android-certs/releasekey --block --backup=true ~/lineage/signed-target_files.zip /var/www/html/builds/$device/lineageOMS-14.1-$todate-$device\.zip
-		
-		# Generate hashes
-		# sha256
-		sha256sum /var/www/html/build/$device/lineageOMS-14.1-$todate-$device\.zip > /var/www/html/build/$device/lineageOMS-14.1-$todate-$device\.zip.sha256
-		# sha1
-		sha1sum /var/www/html/build/$device/lineageOMS-14.1-$todate-$device\.zip > /var/www/html/build/$device/lineageOMS-14.1-$todate-$device\.zip.sha1
-		# md5
-		md5sum /var/www/html/build/$device/lineageOMS-14.1-$todate-$device\.zip > /var/www/html/build/$device/lineageOMS-14.1-$todate-$device\.zip.md5
+	# Sign Build
+	~/lineage/build/tools/releasetools/sign_target_files_apks -o -d ~/.android-certs ~/lineage/out/dist/*-target_files-*.zip ~/lineage/signed-target_files.zip
+	
+	# Generate .zip file
+	~/lineage/build/tools/releasetools/ota_from_target_files -k ~/.android-certs/releasekey --block --backup=true ~/lineage/signed-target_files.zip ~/build/lineageOMS-14.1-$todate-$device.zip
+	
+	# Generate hashes
+	# sha256
+	sha256sum ~/build/lineageOMS-14.1-$todate-$device.zip > ~/build/lineageOMS-14.1-$todate-$device.zip.sha256
+	# sha1
+	sha1sum ~/build/lineageOMS-14.1-$todate-$device.zip > ~/build/lineageOMS-14.1-$todate-$device.zip.sha1
+	# md5
+	md5sum ~/build/lineageOMS-14.1-$todate-$device.zip > ~/build/lineageOMS-14.1-$todate-$device.zip.md5
 
         # Move back to original directory
         cd ~/lineage
@@ -44,8 +37,9 @@ setupenv() {
 
         # Setup build environment
         . build/envsetup.sh
-        unset 'CCACHE_DISABLE' && export 'CCACHE_DISABLE=1'
-		unset 'ANDROID_JACK_VM_ARGS' && export 'ANDROID_JACK_VM_ARGS=-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx8G'		
+	export USE_CCACHE=1
+	prebuilts/misc/linux-x86/ccache/ccache -M 100G
+	export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx8G"
 }
 
 cd /home/builder/lineage
