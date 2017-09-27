9f140e04d10527aff94e92d8e28ccc491b53d955
Wolf <jt.wolf@outlook.com>
2017-09-27 13:42:49 -0400

    add device/[vendor]/[device]/device.mk

---
+++ device/vendor_device_device.mk
@@ -0,0 +1,14 @@
+# Google's Apps
+GAPPS_VARIANT := pico
+
+PRODUCT_PACKAGES += \
+    CalendarGooglePrebuilt \
+    GoogleDialer \
+    Maps \
+    Music2 \
+    Photos \
+    Wallet
+
+GAPPS_FORCE_DIALER_OVERRIDES := true
+
+$(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
