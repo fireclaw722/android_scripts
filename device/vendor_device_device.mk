0ef8ee4409ae5b3a5cb36c9a49e4085b54e29a34
Wolf <jt.wolf@outlook.com>
2017-10-06 00:22:19 -0400

    remove gpm from builds

--- device/vendor_device_device.mk
+++ device/vendor_device_device.mk
@@ -5,12 +5,9 @@ PRODUCT_PACKAGES += \
     CalendarGooglePrebuilt \
     GoogleDialer \
     Maps \
-    Music2 \
     Photos \
     Wallet
 
 GAPPS_FORCE_DIALER_OVERRIDES := true
-
-GAPPS_BYPASS_PACKAGE_OVERRIDES := Music2
 
 $(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
