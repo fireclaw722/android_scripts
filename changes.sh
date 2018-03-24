bcf8fae88d3002d086ffb74eaedcad755ac29575
Jack Wolf <jt.wolf@outlook.com>
2018-03-23 22:46:01 -0400

    blue bootanimation

--- changes.sh
+++ changes.sh
@@ -24,10 +24,13 @@ cd ~/android/lineage/lineage-15.1/vendor/lineage/config/
 cat commons-additions.mk >> common.mk
 
 # Change icon-mask back to square
-# reference: https://github.com/LineageOS/android_vendor_lineage/commit/d12ab12c6142337fc79a76af50fc3d62bc337626
+# revert: https://github.com/LineageOS/android_vendor_lineage/commit/d12ab12c6142337fc79a76af50fc3d62bc337626
 cd ~/android/lineage/lineage-15.1/vendor/lineage/overlay/common/frameworks/base/core/res/res/values/
 sed -r 's/<string name="config_icon_mask" translatable="false">"M50 0A50 50,0,1,1,50 100A50 50,0,1,1,50 0"</string>//' config.xml
 
 # Add unofficial support for updater
 cd ~/android/lineage/lineage-15.1/packages/apps/Updater/res/values/
 sed -r 's/download.lineageos.org/ota.jwolfweb.com/' strings.xml
+
+# add blue bootanimation
+cp ~/Downloads/bootanimation.tar ~/android/lineage/lineage-15.1/vendor/lineage/bootanimation
