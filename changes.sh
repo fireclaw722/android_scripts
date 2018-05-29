7a4bc15353fe113e5f4bbb01c97211af3e32f07d
Jack Wolf <jt.wolf@outlook.com>
2018-05-29 10:10:43 -0400

    changes for supported devices

--- changes.sh
+++ changes.sh
@@ -24,14 +24,10 @@ vendor/lineage/build/tools/repopick.py -f -g https://substratum.review -P build/
 # moto-msm8953/addison
 ### NOT INCLUDED BECAUSE ADDISON ISNT PORTED TO 15.1
 cd ~/android/lineage/lineage-15.1/kernel/motorola/msm8953
-git fetch https://github.com/franciscofranco/one_plus_3T
-git cherry-pick 8e65978cec11a62b0404d88db43adb35f3258e7d c97c758b15ba49bb848e0644089432569145ade3
 
 # moto-msm8952/athene
 ### NOT INCLUDED BECAUSE ATHENE ISNT PORTED TO 15.1
 cd ~/android/lineage/lineage-15.1/kernel/motorola/msm8952
-git fetch https://github.com/franciscofranco/one_plus_3T
-git cherry-pick 8e65978cec11a62b0404d88db43adb35f3258e7d c97c758b15ba49bb848e0644089432569145ade3
 
 # oneplus-msm8996/oneplus3
 cd ~/android/lineage/lineage-15.1/kernel/oneplus/msm8996
@@ -39,6 +35,15 @@ git fetch https://github.com/franciscofranco/one_plus_3T
 git cherry-pick 8e65978cec11a62b0404d88db43adb35f3258e7d c97c758b15ba49bb848e0644089432569145ade3
 
 # moto-msm8974/victara not applicable :: no bootloader check on safetynet
+
+### Remove modemAssertion from device/oneplus/oneplus3/releasetools.py
+    # Replace
+    def IncrementalOTA_Assertions(info):
+        AddModemAssertion(info)
+        return
+    # With
+    def IncrementalOTA_Assertions(info):
+        return
 
 ## Extras
 # add Google Apps, fdroid, and other pre-builts to build process
