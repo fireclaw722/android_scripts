1ded947ea3de24beb6a50c4c997a9f3a30177a89
Jack Wolf <jt.wolf@outlook.com>
2018-05-30 13:27:28 -0400

    rewrite

--- build.bash
+++ build.bash
@@ -1,13 +1,36 @@
 #!/bin/bash
 
 # Variables
-version=15.0.13
-datetime=$(date -u +%Y%m)
-dateforota=$(date -u "+%Y-%m-%d %H:%M:%S")
+version=15.1.0
+datetime=$(date -u +%Y%m%d)
+updaterDate=$(date -u "+%Y-%m-%d %H:%M:%S")
 releasetype=release
 device=
 
-bdevice() {
+showHelp() {
+        echo "Usage: build <device>"
+        echo ""
+        echo "Available devices are:"
+        echo "'oneplus3'"
+        echo ""
+        echo "using the 'help' subcommand shows this text"
+        echo ""
+        echo "using the 'version' subcommand outputs version info"
+}
+
+cleanMka(){
+        cd ~/android/lineage/lineage-15.1
+
+        if ! mka clobber ; then
+                make clobber
+                make clean
+        fi
+}
+
+buildDevice() {
+        # Start clean
+        cleanMka
+
         cd ~/android/lineage/lineage-15.1
 
         # Breakfast
@@ -25,48 +48,67 @@ bdevice() {
 
         # Sign Build
         if ! ./build/tools/releasetools/sign_target_files_apks -o -d ~/.android-certs out/dist/*$device-target_files-*.zip signed-target_files.zip ; then
-                echo "Signing failed."
-                exit
+                echo "Signing failed. Re-input the passwords for second attempt."
+                rm signed-target_files.zip
+                 if ! ./build/tools/releasetools/sign_target_files_apks -o -d ~/.android-certs out/dist/*$device-target_files-*.zip signed-target_files.zip ; then
+                        echo "Signing failed, again. Exiting..."
+                        exit
+                fi
         fi
 
-        # Save Recovery (and boot)
-        ./build/tools/releasetools/img_from_target_files -z signed-target_files.zip /srv/builds/$device/img/Cerulean-15.1-$datetime-$device.zip
-
+        # For some reason, brotli doesn't work unless "otatools" is ran
+        # so build "otatools" for future use
         mka otatools
+}
+
+buildOTA() {
+        cd ~/android/lineage/lineage-15.1
 
         # Package Full OTA
         if ! ./build/tools/releasetools/ota_from_target_files -k ~/.android-certs/releasekey --block --backup=true signed-target_files.zip signed-ota_update.zip ; then
-                echo "Creating OTA .zip failed"
+                echo "Creating Full OTA failed"
                 exit
         fi
-
-        # Save Full OTA
-        mv ~/android/lineage/lineage-15.1/signed-ota_update.zip /srv/builds/$device/full/Cerulean-15.1-$datetime-$device.zip
-
-        # Save target_files
-        mv signed-target_files.zip /srv/builds/$device/target_files/Cerulean-15.1-$datetime-$device.zip
-
-        # Add Full OTA
-        cd ~/updater
-        echo "Adding full OTA to list"
-        FLASK_APP=updater/app.py flask addrom -f Cerulean-15.1-$datetime-$device.zip -d $device -v 15.1 -t "$dateforota" -r $releasetype -s $(stat --printf="%s" /srv/builds/$device/full/Cerulean-15.1-$datetime-$device.zip) -m $(md5sum /srv/builds/$device/full/Cerulean-15.1-$datetime-$device.zip | awk '{ print $1 }') -u https://ota.jwolfweb.com/builds/$device/full/Cerulean-15.1-$datetime-$device.zip
-        cd ~/android/lineage/lineage-15.1
-
-        # Restart the updater
-        cd ~/updater
-        ./run.sh&
-        disown
-
-        cd ~/android/lineage/lineage-15.1
-
-        make clobber
-        mka clobber
 }
 
-setupenv() {
+addOTA() {
+        # Add packaged update to the update backend
+        echo "Adding full OTA to list"
+        cd ~/updater
+
+        FLASK_APP=updater/app.py flask addrom -f Cerulean-15.1-$datetime-$device.zip -d $device -v 15.1 -t "$updaterDate" -r $releasetype -s $(stat --printf="%s" /srv/builds/$device/full/Cerulean-15.1-$datetime.zip) -m $(md5sum /srv/builds/$device/full/Cerulean-15.1-$datetime.zip | awk '{ print $1 }') -u https://ota.jwolfweb.com/builds/$device/full/Cerulean-15.1-$datetime.zip
+
+        # Restart the updater
+        echo "Restarting updater backend..."
+        ./run.sh&
+        disown
+        echo "Updater backend restarted"
+}
+
+saveFiles() {
         cd ~/android/lineage/lineage-15.1
 
-        make clobber
+        # Save Information
+        echo "Build Time for Updater: " >> /srv/builds/$device/target_files/Cerulean-15.1-$datetime.txt
+        echo $updaterDate >> /srv/builds/$device/target_files/Cerulean-15.1-$datetime.txt
+
+        # Save Recovery (and boot)
+        echo "Saving Recovery and Boot Images"
+        ./build/tools/releasetools/img_from_target_files -z signed-target_files.zip /srv/builds/$device/img/Cerulean-15.1-$datetime.zip        
+
+        # Save target_files
+        echo "Saving Build Files"
+        mv signed-target_files.zip /srv/builds/$device/target_files/Cerulean-15.1-$datetime.zip
+
+        # Save OTA files
+        echo "Saving OTA update"
+        mv signed-ota_update.zip /srv/builds/$device/full/Cerulean-15.1-$datetime.zip
+}
+
+setupEnv() {
+        cd ~/android/lineage/lineage-15.1
+
+        cleanMka
 
         # Setup build environment
         source build/envsetup.sh
@@ -84,29 +126,30 @@ if [ $# -gt 0 ]; then
 
                         cd ~/android/lineage/lineage-15.1
 
-                        setupenv
+                        setupEnv
 
-                        bdevice
+                        buildDevice
+
+                        buildOTA
+
+                        saveFiles
+
+                        addOTA
+
+                        cleanMka
                         ;;
                 help|-h|--help)
-                        echo "Usage: build <device>"
-                        echo ""
-                        echo "Available devices are:"
-                        echo "'oneplus3'"
-                        echo ""
-                        echo "using the 'help' subcommand shows this text"
-                        echo ""
-                        echo "using the 'version' subcommand outputs version info"
+                        showHelp
                         ;;
                 version|-v|--version)
                         echo "Version: "$version
                         ;;
                 *)
-                        echo "Codename not available for build."
+                        echo "Error: Codename not available for build."
                         echo ""
-                        build-15 help
+                        showHelp
         esac
 else
-        echo "Please use a codename for the device you wish to build."
-        build help
+        echo "Error: Please use a codename for the device you wish to build."
+        showHelp
 fi
