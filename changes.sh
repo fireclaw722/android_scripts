ec7ccf77ab71cba61a4578d11fe60b8a77e39d96
Jack Wolf <jt.wolf@outlook.com>
2018-05-30 15:03:08 -0400

    update paths

--- changes.sh
+++ changes.sh
@@ -23,14 +23,14 @@ vendor/lineage/build/tools/repopick.py -f -g https://substratum.review -P build/
 ## Add SafetyNet Patches
 # moto-msm8953/addison
 ### NOT INCLUDED BECAUSE ADDISON ISNT PORTED TO 15.1
-cd ~/android/lineage/lineage-15.1/kernel/motorola/msm8953
+cd ~/android/cerulean/oreo-mr1/kernel/motorola/msm8953
 
 # moto-msm8952/athene
 ### NOT INCLUDED BECAUSE ATHENE ISNT PORTED TO 15.1
-cd ~/android/lineage/lineage-15.1/kernel/motorola/msm8952
+cd ~/android/cerulean/oreo-mr1/kernel/motorola/msm8952
 
 # oneplus-msm8996/oneplus3
-cd ~/android/lineage/lineage-15.1/kernel/oneplus/msm8996
+cd ~/android/cerulean/oreo-mr1/kernel/oneplus/msm8996
 git fetch https://github.com/franciscofranco/one_plus_3T
 git cherry-pick 8e65978cec11a62b0404d88db43adb35f3258e7d c97c758b15ba49bb848e0644089432569145ade3
 
@@ -47,31 +47,31 @@ git cherry-pick 8e65978cec11a62b0404d88db43adb35f3258e7d c97c758b15ba49bb848e064
 
 ## Extras
 # add Google Apps, fdroid, and other pre-builts to build process
-cd ~/android/lineage/lineage-15.1/vendor/lineage/config/
+cd ~/android/cerulean/oreo-mr1/vendor/lineage/config/
 cat commons-additions.mk >> common.mk
 
 # Change icon-mask back to square
 # revert: https://github.com/LineageOS/android_vendor_lineage/commit/d12ab12c6142337fc79a76af50fc3d62bc337626
-cd ~/android/lineage/lineage-15.1/vendor/lineage/overlay/common/frameworks/base/core/res/res/values/
+cd ~/android/cerulean/oreo-mr1/vendor/lineage/overlay/common/frameworks/base/core/res/res/values/
 sed -r 's/<string name="config_icon_mask" translatable="false">"M50 0A50 50,0,1,1,50 100A50 50,0,1,1,50 0"</string>//' config.xml
 
 # Add support for unofficial updates
-cd ~/android/lineage/lineage-15.1/packages/apps/Updater/res/values/
+cd ~/android/cerulean/oreo-mr1/packages/apps/Updater/res/values/
 sed -r 's/download.lineageos.org/ota.jwolfweb.com/' strings.xml
 
 # add blue bootanimation
-cp ~/Downloads/blue-los-bootanimation.tar ~/android/lineage/lineage-15.1/vendor/lineage/bootanimation/bootanimation.tar
-cd ~/android/lineage/lineage-15.1/vendor/lineage/bootanimation/
+cp ~/Downloads/blue-los-bootanimation.tar ~/android/cerulean/oreo-mr1/vendor/lineage/bootanimation/bootanimation.tar
+cd ~/android/cerulean/oreo-mr1/vendor/lineage/bootanimation/
 
 # Cerulean Rebrand
-cd ~/android/lineage/lineage-15.1/lineage-sdk/lineage/res/res/values/
+cd ~/android/cerulean/oreo-mr1/lineage-sdk/lineage/res/res/values/
 sed -r 's/LineageOS/Cerulean/' strings.xml
 
-cd ~/android/lineage/lineage-15.1/vendor/lineage/config/
+cd ~/android/cerulean/oreo-mr1/vendor/lineage/config/
 sed -r 's/LineageOS/Cerulean/' common.mk
 
-cd ~/android/lineage/lineage-15.1/packages/apps/Updater/res/values/
+cd ~/android/cerulean/oreo-mr1/packages/apps/Updater/res/values/
 sed -r 's/LineageOS/Cerulean/' strings.xml
 
-cd ~/android/lineage/lineage-15.1/packages/apps/LineageParts/res/values
+cd ~/android/cerulean/oreo-mr1/packages/apps/LineageParts/res/values
 sed -r 's/LineageOS/Cerulean/' strings.xml
