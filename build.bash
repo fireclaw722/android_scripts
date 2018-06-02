ee8f39a9421b3e7f5013c0b0f7d631e6e4f0e215
Jack Wolf <jt.wolf@outlook.com>
2018-06-02 12:06:03 -0400

    fix adding ota

--- build.bash
+++ build.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Variables
-version=0.2.1
+version=0.2.2
 builddate=$(date -u +%Y%m%d)
 updaterDate=$(date -u "+%Y-%m-%d %H:%M:%S")
 releasetype=release
@@ -90,10 +90,10 @@ addOTA() {
         cd ~/updater
 
         if [ "$releasetype" == "nightly" ] ; then
-                FLASK_APP=updater/app.py flask addrom -f "15.1-beta $(date --date='$updaterDate' +%Y-%m-%d)" -d $device -v 15.1 -t "$updaterDate" -r $releasetype -s $(stat --printf="%s" /srv/builds/$device/full/Cerulean-15.1.$builddate.zip) -m $(md5sum /srv/builds/$device/full/Cerulean-15.1.$builddate.zip | awk '{ print $1 }') -u https://ota.jwolfweb.com/builds/$device/full/Cerulean-15.1.$builddate.zip
+                FLASK_APP=updater/app.py flask addrom -f "15.1-beta $(date --date="$updaterDate" +%Y-%m-%d)" -d $device -v 15.1 -t "$updaterDate" -r $releasetype -s $(stat --printf="%s" /srv/builds/$device/full/Cerulean-15.1.$builddate.zip) -m $(md5sum /srv/builds/$device/full/Cerulean-15.1.$builddate.zip | awk '{ print $1 }') -u https://ota.jwolfweb.com/builds/$device/full/Cerulean-15.1.$builddate.zip
                 echo "Full beta OTA added"
         elif [ "$releasetype" == "release" ] ; then
-                FLASK_APP=updater/app.py flask addrom -f "15.1 $(date --date='$updaterDate' +%Y-%m)" -d $device -v 15.1 -t "$updaterDate" -r $releasetype -s $(stat --printf="%s" /srv/builds/$device/full/Cerulean-15.1.$builddate.zip) -m $(md5sum /srv/builds/$device/full/Cerulean-15.1.$builddate.zip | awk '{ print $1 }') -u https://ota.jwolfweb.com/builds/$device/full/Cerulean-15.1.$builddate.zip
+                FLASK_APP=updater/app.py flask addrom -f "15.1 $(date --date="$updaterDate" +%Y-%m)" -d $device -v 15.1 -t "$updaterDate" -r $releasetype -s $(stat --printf="%s" /srv/builds/$device/full/Cerulean-15.1.$builddate.zip) -m $(md5sum /srv/builds/$device/full/Cerulean-15.1.$builddate.zip | awk '{ print $1 }') -u https://ota.jwolfweb.com/builds/$device/full/Cerulean-15.1.$builddate.zip
                 echo "Full release OTA added"
         fi
 
