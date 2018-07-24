c84bbdbac6f8e7ce9b8b18f8d2aa2bcb5f230647
Jack <jackw@preblelibrary.org>
2018-07-24 17:38:14 -0400

    fix filenames

--- build.bash
+++ build.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Variables
-version=0.6.1
+version=0.6.2
 device=
 builddate=
 updaterDate=
@@ -120,7 +120,7 @@ addOTA() {
         echo ""
         cd ~/updater
 
-        FLASK_APP=updater/app.py flask addrom -f $RomName-$RomVers.$builddate-$device -d $device -v $RomVers -t "$updaterDate" -r $releasetype -s $(stat --printf="%s" /srv/builds/$device/full/$RomName-$RomVers.$builddate.zip) -m $(md5sum /srv/builds/$device/full/$RomName-$RomVers.$builddate.zip | awk '{ print $1 }') -u https://ota.jwolfweb.com/builds/$device/full/Cerulean-8.1.$builddate.zip
+        FLASK_APP=updater/app.py flask addrom -f $RomName-$RomVers.$builddate-$device -d $device -v $RomVers -t "$updaterDate" -r $releasetype -s $(stat --printf="%s" /srv/builds/$device/full/$RomName-$RomVers.$builddate-$device.zip) -m $(md5sum /srv/builds/$device/full/$RomName-$RomVers.$builddate-$device.zip | awk '{ print $1 }') -u https://ota.jwolfweb.com/builds/$device/full/Cerulean-8.1.$builddate-$device.zip
 
         echo "Full OTA added"
 
