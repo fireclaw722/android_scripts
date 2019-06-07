271ff1238b06b336a5aad7a27dc21cc59dd01933
fireclaw <jt.wolf@outlook.com>
2019-06-07 15:55:36 -0400

    LC_ALL set first

--- build.bash
+++ build.bash
@@ -42,7 +42,7 @@ setupEnv() {
         else
                 exit
         fi
-        export builddate=$(date --date="4 hours ago" -u +%Y%m%d)
+        export LC_ALL=C builddate=$(date --date="4 hours ago" -u +%Y%m%d)
 
         export USE_CCACHE=0 CCACHE_DISABLE=1 ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx8G" LC_ALL=C fileName=$RomName-$RomVers-$builddate-$releasetype-$device
 }
