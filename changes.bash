e04a67cffd6f0aecbc2da572393d175896cd2c17
fireclaw722 <jt.wolf@outlook.com>
2020-03-10 16:28:42 -0400

    new fingerprint

--- changes.bash
+++ changes.bash
@@ -35,21 +35,22 @@ cd ~/android/lineage/17.1/kernel/google/msm-4.9
 git fetch "https://github.com/LineageOS/android_kernel_google_msm-4.9" refs/changes/27/263927/2 && git cherry-pick FETCH_HEAD
 
 # Sargo Bluetooth issue
+cd ~/android/lineage/17.1/
 vendor/lineage/build/tools/repopick.py 268545 -P device/google/bonito/
 vendor/lineage/build/tools/repopick.py 270242 -P device/google/bonito/
 vendor/lineage/build/tools/repopick.py 268686 -P device/google/bonito/
 
 # Sargo Performance improvements
-cd  ~/android/lineage/17.1/kernel/google/msm-4.9
+cd ~/android/lineage/17.1/kernel/google/msm-4.9
 git fetch "https://github.com/LineageOS/android_kernel_google_msm-4.9" refs/changes/40/263940/1 && git cherry-pick FETCH_HEAD
 
 # Bonito/Sargo update fingerprint
-cd ~/android/lineage/17.1/device/google/bonito
-git fetch https://github.com/invisiblek/android_device_google_bonito lineage-17.1_btwtf
-git cherry-pick 1d50d687d6d5ab027ad98e53e999f14fa9aa1d6d
+cd ~/android/lineage/17.1/
+vendor/lineage/build/tools/repopick.py 270099 -P device/google/bonito/
+vendor/lineage/build/tools/repopick.py 270100 -P device/google/bonito/
 
 # google_msm-4.9 Safetynet
-cd  ~/android/lineage/17.1/kernel/google/msm-4.9
+cd ~/android/lineage/17.1/kernel/google/msm-4.9
 git fetch https://github.com/flar2/Bluecross 
 git cherry-pick 27823dd5b1bc55f6ae3b09bcd321d2e2614c28a1
 git cherry-pick 978aa6865137c3a7cf7c05415dbeb30ad3e82052
