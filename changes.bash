3438e215570d1cdb26ff3dbf4dc04c803ffb634d
fireclaw722 <jt.wolf@outlook.com>
2020-09-24 16:15:24 -0400

    support other lineage versions

--- changes.bash
+++ changes.bash
@@ -9,10 +9,58 @@ exit
 ##
 cd ~/android/lineage/15.1
 
+### System-based changes
+
+# add pre-built apps to build process (see commons-addition.mk for options)
+cd ~/android/lineage/15.1/vendor/lineage/config/
+vi common.mk
+
+# Blue Bootanimation
+cd ~/android/lineage/15.1/vendor/lineage/bootanimation
+cp ~/Downloads/bootanimation.tar ./
+
+# Updater URL
+cd ~/android/lineage/15.1/packages/apps/Updater/res/values/
+vi strings.xml
+
+# MicroG Signature-Spoofing support
+cd ~/android/lineage/15.1/
+patch --no-backup-if-mismatch --strip='1' --directory=frameworks/base < ~/Downloads/GmsCore-android_frameworks_base-O.patch
+cd ~/android/lineage/15.1/frameworks/base
+git commit
+
+# Update for new security patches
+vendor/lineage/build/tools/repopick.py -g https://review.lineageos.org -t #O_asb_XXXX-XX
+
+### Device-based changes
+# Moto Mod Battery Efficiency Mode (run twice because it doesn't merge cleanly)
+cd ~/android/lineage/15.1
+vendor/lineage/build/tools/repopick.py -f -g https://review.lineageos.org -t moto-mods-battery-lineage-15.1
+
 ##
 ## Android 9 / Lineage 16.0 (Pie)
 ##
 cd ~/android/lineage/16.0
+
+### System-based changes
+
+# add pre-built apps to build process (see commons-addition.mk for options)
+cd ~/android/lineage/16.0/vendor/lineage/config/
+vi common.mk
+
+# Blue Bootanimation
+cd ~/android/lineage/16.0/vendor/lineage/bootanimation
+cp ~/Downloads/bootanimation.tar ./
+
+# For MicroG Signature-Spoofing support (for reference)
+cd ~/android/lineage/16.0/
+patch --no-backup-if-mismatch --strip='1' --directory=frameworks/base < ~/Downloads/GmsCore-android_frameworks_base-P.patch
+cd ~/android/lineage/16.0/frameworks/base
+git commit
+
+# Updater URL
+cd ~/android/lineage/16.0/packages/apps/Updater/res/values/
+vi strings.xml
 
 ##
 ## Android 10 / Lineage 17.1 (Q)
@@ -55,3 +103,24 @@ git cherry-pick 978aa6865137c3a7cf7c05415dbeb30ad3e82052
 ## Android 11 / Lineage 18.0 (R)
 ##
 cd ~/android/lineage/18.0
+
+### System-based changes
+
+# add pre-built apps to build process (see commons-addition.mk for options)
+cd ~/android/lineage/18.0/vendor/lineage/config/
+vi common.mk
+
+# Blue Bootanimation
+cd ~/android/lineage/18.0/vendor/lineage/bootanimation
+cp ~/Downloads/bootanimation.tar ./
+
+# For MicroG Signature-Spoofing support (for reference)
+# DOES NOT EXIST YET
+#cd ~/android/lineage/18.0/
+#patch --no-backup-if-mismatch --strip='1' --directory=frameworks/base < ~/Downloads/GmsCore-android_frameworks_base-P.patch
+#cd ~/android/lineage/18.0/frameworks/base
+#git commit
+
+# Updater URL
+cd ~/android/lineage/18.0/packages/apps/Updater/res/values/
+vi strings.xml
