8fd096405eb92db865413cb2c1efee0630e22787
JT Wolf <jt.wolf@outlook.com>
2021-12-22 15:55:30 -0500

    Use ProtonAOSP as base for GMSCompat

--- changes.bash
+++ changes.bash
@@ -89,21 +89,59 @@ cd ~/android/lineage/18.1/packages/apps/Updater/res/values/
 vi strings.xml
 
 # GMS Compat from ProtonAOSP and GrapheneOS
-# pulled from https://github.com/sn-00-x/lineage-gmscompat
+# pulled from https://github.com/ProtonAOSP/android_frameworks_base/commit/05c0673d57ea9183eb04ef31573a96a9306c3b74
 cd ~/android/lineage/18.1/frameworks/base
-for i in ~/Downloads/lineage-gmscompat/src/gmscompat_patches/frameworks/base/*.patch ; do
-    patch --quiet --force -p1 < $i
-done
-
-cd ~/android/lineage/18.1/libcore
-for i in ~/Downloads/lineage-gmscompat/src/gmscompat_patches/libcore/*.patch ; do
-    patch --quiet --force -p1 < $i
-done
+git fetch https://github.com/ProtonAOSP/android_frameworks_base rvc
+git cherry-pick caf68a608b9806c8c9bed6003c5770fc9fe39113
+git cherry-pick 1f5b60e686a1bb60611ce510509abc0a5af23bcc
+git cherry-pick 31f39f24fca258fe0a6d467f21ee40cdca331014
+git cherry-pick 101e4f994635a77056135d87ac511e5b2e585725
+git cherry-pick bee11e7cb2bd8fa9f1a74461dec5881b57146520
+git cherry-pick 8768d3d4741fef1c190f1d9f2f765091453ef78c
+git cherry-pick 946d5f2aac01b6b56b629c56ea39d7f9838d790b
+git cherry-pick ed40fef2cd80afe1cd042f7d7fd512598d55fcb6
+# fix merge error
+# remove line containing "OTHER_SENSORS" AS WELL AS the following line
+## That is a GrapheneOS and ProtonAOSP addition
+git cherry-pick 7a420d2e0f5a710d194c714d111402e021379617
+git cherry-pick d59f9e0c7401484d5a617e55900a6b474a94a21e
+git cherry-pick ac61e010f27ad4f9f9bdacbfad824909dc219883
+# fix merge error
+# remove ONE of the lines containing "newPkg.isForceQueryable()"
+# remove ONE of the lines containing "newPkgSetting.forceQueryableOverride"
+## keep the other (of both)
+git cherry-pick 100407a57ef4018a507931e51d80c15bed2b153a
+git cherry-pick 9723fa311dc82c546028f20f1c6766692bc36a59
+git cherry-pick 69cc21f538305f6d3024d86de3690bb313f5eb1b
+git cherry-pick f9e45fd421716de5c91ac744daf9118cb75a5697
+git cherry-pick 94b0be463149f8020aacd21ac9da9b98b53e7e44
+git cherry-pick a83c73765827e3da282b34a62c0e5ca479d92b56
+git cherry-pick 0ff2739056f34a61b4f682ae5781151c575c58fe
+git cherry-pick 39b5ecdd0387c1e918cc77a31f15b52364dc15a5
+git cherry-pick 9d7f4e47e882010005f3b668911c009c586ce526
+git cherry-pick 8fe750a9d3484467e75222c11aac5973a06e53fd
+git cherry-pick 4593203ba079f9590fa319585dcf24c9488d3b09
+# fix merge error
+# remove lines containing "maybeSpoofBuild(app);"
+git cherry-pick 2f70981714393593b733d78522c83b7323f5a891
+git cherry-pick c965caa7a0dd4d4f561b66c58484deda04cd6791
+git cherry-pick b4811718bf68108fb6419bf0d09579536373e008
+git cherry-pick 68603ce63a90d452f233731e4ddf9b12d68c1749
+git cherry-pick 3aff70d2b02cc68540208f132d9f8178ecc4c2f9
+git cherry-pick 2646990f914c8ad38f925bfd73249b9b803e229f
+git cherry-pick 02044c1fb2c740f180a77bec5896a61a37325b96
+# fix merge error
+# Same issue as ac61e010f27ad4f9f9bdacbfad824909dc219883
+# fix the same way; remove the extra calls
 
 cd ~/android/lineage/18.1/bionic
-for i in ~/Downloads/lineage-gmscompat/src/gmscompat_patches/bionic/*.patch ; do
-    patch --quiet --force -p1 < $i
-done
+git fetch https://github.com/ProtonAOSP/android_bionic rvc
+git cherry-pick bc443b6de8343858d2cb2272bc840f4c519ab2de
+
+cd ~/android/lineage/18.1/libcore
+git fetch https://github.com/ProtonAOSP/android_libcore rvc
+git cherry-pick af740ed9fae7d6ec494b98b7eb6960ccd49b4076
+git cherry-pick 55f4cb6cd74191178263a492fecab2419c19715f
 
 ### Device-specfic commits
 # Comment out reserved space for GApps if you build w/ GApps
